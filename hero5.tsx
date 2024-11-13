dc87f0c468c537afa1efff8a678a7b045adf02cc
AnasAhmed5000 <fazoolbandaclass@gmail.com>
2024-11-12 17:46:38 -0800

    Add files via upload

---
+++ hero5.tsx
@@ -0,0 +1,17 @@
+  {/* Section Five */}
+  const Hero5 =() =>{
+    return(
+        <div className=" w-[1920px] h-[574px] py-[140px] px-[220px] bg-[#043873] text-[#FFFFFF] text-center items-center justify-between ">
+            <div className="w-[1481px] h-[294px] ">
+                <div className="w-[1064px] h-[171px] gap-6 ">
+                    <h1 className="w-[1064px] h-[87px] font-bold text-[70px] leading-[87.14px] tracking-[0.02em] ">Your work, everywhere you are</h1>
+                    <p className=" w-[1064px] h-[60px] font-normal text-lg leading-[30px] tracking-[0.02em] items-center mt-4 ">Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace, Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!</p>
+            </div>
+             
+                  <button className="w-[195-px] h-[63px] bg-[#4F9CF9] py-5 px-10 rounded-lg text-center ">Try Taskey</button>
+                  </div>
+             </div>
+
+    )
+}
+export default Hero5
