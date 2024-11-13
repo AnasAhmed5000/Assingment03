c7173b82f785e725906e7864f741f630215db236
AnasAhmed5000 <fazoolbandaclass@gmail.com>
2024-11-12 17:47:16 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,33 @@
+
+import Navbar from "./components/navbar"
+import Hero from "./components/hero"
+import Hero2 from "./components/hero2"
+import Hero3 from "./components/hero3"
+ import Hero4 from "./components/hero4"
+ import Hero5 from "./components/hero5"
+ import Hero6 from "./components/hero6"
+ import Footer from "./components/footer"
+ 
+
+export default function Hero7 () {
+  return(
+     <div>
+      
+     <Navbar />
+     <Hero />
+     <Hero2 />
+     <Hero3 />
+     <Hero4 />
+     <Hero5 />
+     <Hero6 />
+     <Footer />
+     
+     
+     
+    </div>
+      
+      
+ 
+  )
+}
+
